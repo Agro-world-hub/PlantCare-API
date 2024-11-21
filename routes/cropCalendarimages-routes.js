004cfc82cc61d345c27b941173d0e60ac3d6310e
KHRDilshan <ravindilshan1234@gmail.com>
2024-11-21 13:04:27 +0530

    update

--- routes/cropCalendarimages-routes.js
+++ routes/cropCalendarimages-routes.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const { uploadImage ,upload} = require('../end-point/cropCalendarimages-ep');
-
-const { uploadImage } = require('../end-point/cropCalendarimages-ep');
+const  uploadImage  = require('../end-point/cropCalendarimages-ep');
 const upload = require('../Middlewares/multer.middleware');
-
 
 
 // Initialize the router
 const router = express.Router();
 
 // Define the route for uploading an image
-router.post('/calendar-tasks/upload-image', upload.single('image'), uploadImage);
+router.post('/calendar-tasks/upload-image', upload.single('image'), uploadImage.uploadImage);
 
 module.exports = router;
