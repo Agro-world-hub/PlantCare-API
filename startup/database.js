4f1dc0d2cba640f9b743a6405b8707c0258d0808
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2025-01-30 20:48:56 +0530

    Refactor database connections to use connection pools for improved performance and error handling

--- startup/database.js
+++ startup/database.js
@@ -1,40 +1,63 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-
-const plantcare = mysql.createConnection({
+const plantcare = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_PC,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
 
-const collectionofficer = mysql.createConnection({
+const collectionofficer = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_CO,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
-const marketPlace = mysql.createConnection({
+const marketPlace = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_MP,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
 
-const dash = mysql.createConnection({
+const dash = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME_DS,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 6, 
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay : 0,
 });
 
 
-module.exports = {plantcare, collectionofficer, marketPlace, dash};
+module.exports = { plantcare, collectionofficer, marketPlace, dash};
