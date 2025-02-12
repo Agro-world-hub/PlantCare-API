b43903d3272561cbba37d3a075f77b494aefdcf7
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2025-02-12 13:30:07 +0530

    Refactor complaint-related endpoints and implement database connection pooling

--- startup/database.js
+++ startup/database.js
@@ -1,90 +1,6 @@
-// const mysql = require('mysql2');
-// require('dotenv').config();
-
-
-// const plantcare = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME_PC,
-//   charset: 'utf8mb4',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 6, 
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay : 0,
-// });
-
-
-// const collectionofficer = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME_CO,
-//   charset: 'utf8mb4',
-//   charset: 'utf8mb4',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 6, 
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay : 0,
-// });
-
-// const marketPlace = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME_MP,
-//   charset: 'utf8mb4',
-//   charset: 'utf8mb4',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 6, 
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay : 0,
-// });
-
-
-// const dash = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME_DS,
-//   charset: 'utf8mb4',
-//   charset: 'utf8mb4',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 6, 
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay : 0,
-// });
-
-// const admin = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME_AD,
-//   charset: 'utf8mb4',
-//   charset: 'utf8mb4',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 6, 
-//   queueLimit: 0,
-//   enableKeepAlive: true,
-//   keepAliveInitialDelay : 0,
-// });
-
-
-// module.exports = {plantcare, collectionofficer, marketPlace, dash, admin};
-
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Creating the database connection pools for each service
 const createPool = (dbName) => {
   return mysql.createPool({
     host: process.env.DB_HOST,
@@ -97,7 +13,7 @@ const createPool = (dbName) => {
     maxIdle: 6, 
     queueLimit: 0,
     enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
+    keepAliveInitialDelay : 0,
   });
 };
 
