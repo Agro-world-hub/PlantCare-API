bd13f76475cc7c4cc24258b7bf5a8b2e04534f45
Tharaka Nuwan <tnathuluwage@gmail.com>
2025-04-18 16:59:30 +0530

    new GRN api functions

--- server.js
+++ server.js
@@ -179,6 +179,9 @@ app.use(process.env.AUTHOR, publicforumRoutes);
 const calendartaskImages = require("./routes/cropCalendarimages-routes");
 app.use(process.env.AUTHOR, calendartaskImages);
 
+const reportRoutes = require("./routes/reportRoutes");
+app.use(process.env.AUTHOR, reportRoutes);
+
 app.use("/api/news", newsRoutes);
 app.use("/api/crop", cropRoutes);
 app.use("/api/market-price", MarketPriceRoutes);
